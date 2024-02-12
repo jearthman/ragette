7f157c6281e4a68512968ce3476c84ea456effef
jearthman <james.earthman@gmail.com>
2024-02-12 11:18:44 -0600

    Add batch insertion for AstraDB

--- app/api/embed/vectorization.ts
+++ app/api/embed/vectorization.ts
@@ -5,6 +5,12 @@ import { OpenAIEmbeddings } from "@langchain/openai";
 
 const { ASTRA_DB_APPLICATION_TOKEN, ASTRA_DB_API_ENDPOINT } = process.env;
 
+type AstraDoc = {
+  fileId: string;
+  text: string;
+  $vector: number[];
+};
+
 export default async function vectorizeFile(file: Blob, fileId: string) {
   try {
     const loader = await LoaderFactory(file);
@@ -35,19 +41,27 @@ export default async function vectorizeFile(file: Blob, fileId: string) {
 
     // console.log("Document embeddings: ", JSON.stringify(documentEmbeddings));
 
-    const astraDocs = splitStrings.map((splitString, index) => ({
+    const astraDocs: AstraDoc[] = splitStrings.map((splitString, index) => ({
       fileId: fileId,
       text: splitString,
       $vector: documentEmbeddings[index],
     }));
 
-    console.log("Astra docs: ", JSON.stringify(astraDocs));
+    // console.log("Astra docs: ", JSON.stringify(astraDocs));
+
+    console.log("Inserting into Astra");
 
     const db = new AstraDB(ASTRA_DB_APPLICATION_TOKEN, ASTRA_DB_API_ENDPOINT);
 
     const collection = await db.collection("ragette_cosine");
 
-    const res = await collection.insertMany(astraDocs);
+    const batches = chunkArray(astraDocs, 20);
+
+    const batchesReq = batches.map((batch) => {
+      return collection.insertMany(batch);
+    });
+
+    const res = await Promise.all(batchesReq);
 
     console.log("Inserted: ", res);
 
@@ -56,4 +70,25 @@ export default async function vectorizeFile(file: Blob, fileId: string) {
     console.log("Error: ", error);
     throw error;
   }
+}
+
+/**
+ * Chunk an array into smaller arrays of a specified size.
+ *
+ * @param arr - The array to be chunked.
+ * @param chunkSize - The size of each chunk.
+ * @returns An array of smaller arrays, each containing elements from the original array.
+ */
+function chunkArray(arr: AstraDoc[], chunkSize: number): AstraDoc[][] {
+  return arr.reduce((chunks: AstraDoc[][], elem, index) => {
+    // Calculate the index of the chunk that the current element should be placed in.
+    const chunkIndex = Math.floor(index / chunkSize);
+    // Create the chunk if it doesn't exist.
+    if (!chunks[chunkIndex]) {
+      chunks[chunkIndex] = [];
+    }
+    // Add the element to the chunk.
+    chunks[chunkIndex].push(elem);
+    return chunks;
+  }, []);
 }
