e4347ded7c4451651fb65f3780e09bb1523d8b35
jearthman <james.earthman@gmail.com>
2024-03-02 13:27:14 -0600

    Update dependencies and include new files

---
+++ app/api/chat/nodes.js
@@ -0,0 +1,189 @@
+import { OpenAIEmbeddings, ChatOpenAI } from "@langchain/openai";
+import { Pinecone } from "@pinecone-database/pinecone";
+import { z } from "Zod";
+import { convertToOpenAITool } from "@langchain/core/utils/function_calling";
+import { DynamicStructuredTool } from "@langchain/core/tools";
+import { StringOutputParser } from "@langchain/core/output_parsers";
+import { GradingPrompt, TransformPrompt } from "./prompt-templates";
+import { TavilySearchResults } from "@langchain/community/tools/tavily_search";
+
+const pc = new Pinecone();
+
+// NODES
+
+export async function retrieve(state) {
+  try {
+    let { question, fileId, retryCount } = state;
+
+    if (!retryCount) {
+      retryCount = 1;
+    } else {
+      retryCount++;
+    }
+
+    const embeddings = new OpenAIEmbeddings();
+    const embeddedQuery = await embeddings.embedQuery(question);
+    console.log(`Embedded query: ${embeddedQuery}`);
+
+    // Query Pinecone for the most relevant documents.
+    const namespace = pc.index("ragette").namespace(fileId);
+    const queryRes = await namespace.query({
+      topK: 5,
+      vector: embeddedQuery,
+      includeMetadata: true,
+    });
+    console.log(`Query Res: ${JSON.stringify(queryRes)}`);
+    // Extract the context from the most relevant documents.
+    const docs = queryRes.matches
+      .map((record) =>
+        record.metadata && record.metadata.text
+          ? record.metadata.text.toString()
+          : "",
+      )
+      .filter((text) => text.length > 0);
+
+    return {
+      ...state,
+      docs: docs,
+      retryCount: retryCount,
+    };
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function grade(state) {
+  try {
+    const { question, docs } = state;
+
+    // const grade = z.object({
+    //   binary_score: z
+    //     .string()
+    //     .refine((value) => value === "yes" || value === "no", {
+    //       message: "Relevance score must be 'yes' or 'no'",
+    //     }),
+    // });
+
+    // const tool = new DynamicStructuredTool({
+    //   name: "relevance-check",
+    //   description: "Binary score for relevance check",
+    //   schema: grade,
+    //   func: async ({ binary_score }) => {
+    //     return binary_score;
+    //   },
+    // });
+
+    const model = new ChatOpenAI({
+      temperature: 0,
+      modelName: "gpt-4-0125-preview",
+      streaming: true,
+    });
+
+    // const gradingTool = convertToOpenAITool(tool);
+
+    // const llmWithTool = model.bind({ tools: [gradingTool] });
+
+    const prompt = GradingPrompt;
+
+    const chain = prompt.pipe(model);
+
+    const filteredDocs = [];
+    let tramsformQuery = false;
+
+    for (const doc of docs) {
+      const result = await chain.invoke({
+        context: doc,
+        question: question,
+      });
+      const grade = result.content.toLowerCase();
+      if (grade === "yes") {
+        filteredDocs.push(doc);
+        break;
+      }
+    }
+
+    if (filteredDocs.length === 0) {
+      tramsformQuery = true;
+    }
+
+    return {
+      ...state,
+      docs: filteredDocs,
+      tramsformQuery: tramsformQuery,
+    };
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function transformQuery(state) {
+  try {
+    const { question } = state;
+
+    const prompt = TransformPrompt;
+
+    const model = new ChatOpenAI({
+      temperature: 0,
+      modelName: "gpt-4-0125-preview",
+      streaming: true,
+    });
+
+    const outputParser = new StringOutputParser();
+
+    const chain = prompt.pipe(model).pipe(outputParser);
+
+    const betterQuestion = await chain.invoke({ question: question });
+
+    return {
+      ...state,
+      question: betterQuestion,
+    };
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function webSearch(state) {
+  try {
+    const { question, docs } = state;
+
+    const tool = new TavilySearchResults({ maxResults: 1 });
+
+    const webDoc = await tool.invoke(question);
+
+    docs.push(webDoc);
+
+    return {
+      ...state,
+      docs: docs,
+    };
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// EDGES
+
+export function decideToChat(state) {
+  try {
+    if (state.tramsformQuery) {
+      return "transform_query";
+    } else {
+      return "END";
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export function decideToRetrieve(state) {
+  try {
+    if (state.retryCount >= 1) {
+      return "web_search";
+    }
+
+    return "retrieve";
+  } catch (error) {
+    console.log(error);
+  }
+}
--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,14 +1,36 @@
 import OpenAI from "openai";
 import { OpenAIStream, StreamingTextResponse } from "ai";
-import { OpenAIEmbeddings } from "@langchain/openai";
 import { Pinecone } from "@pinecone-database/pinecone";
 import { QuestionCheckPrompt, SystemPrompt } from "./prompt-templates";
-
-const pc = new Pinecone();
+import { StateGraph, END } from "@langchain/langgraph";
+import * as nodes from "./nodes";
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
+
+const ragState = {
+  question: {
+    value: (x: string, y: string) => (x = y),
+    default: () => "",
+  },
+  docs: {
+    value: (x: string[], y: string[]) => (x = y),
+    default: () => [],
+  },
+  fileId: {
+    value: (x: string, y: string) => (x = y),
+    default: () => "",
+  },
+  retryCount: {
+    value: (x: number, y: number) => (x = y),
+    default: () => 0,
+  },
+  tramsformQuery: {
+    value: (x: boolean, y: boolean) => (x = y),
+    default: () => false,
+  },
+};
 
 /**
  * Handles the POST request for the chat route.
@@ -42,24 +64,36 @@ export async function POST(req: Request) {
   if (
     questionCheckRes.choices.pop()?.message.content?.toLowerCase() === "yes"
   ) {
-    // Embed the user query using OpenAI.
-    const embeddings = new OpenAIEmbeddings();
-    const embeddedQuery = await embeddings.embedQuery(lastUserMessage.content);
-    console.log(`Embedded query: ${embeddedQuery}`);
+    // // Embed the user query using OpenAI.
+    // const embeddings = new OpenAIEmbeddings();
+    // const embeddedQuery = await embeddings.embedQuery(lastUserMessage.content);
+    // console.log(`Embedded query: ${embeddedQuery}`);
 
-    // Query Pinecone for the most relevant documents.
-    const namespace = pc.index("ragette").namespace(fileId);
-    const queryRes = await namespace.query({
-      topK: 10,
-      vector: embeddedQuery,
-      includeMetadata: true,
-    });
-    console.log(`Query Res: ${JSON.stringify(queryRes)}`);
-    // Extract the context from the most relevant documents.
-    context = queryRes.matches
-      .map((record) => record.metadata?.text)
-      .join("\n");
-    console.log(`Retrieved documents: ${context}`);
+    // // Query Pinecone for the most relevant documents.
+    // const namespace = pc.index("ragette").namespace(fileId);
+    // const queryRes = await namespace.query({
+    //   topK: 10,
+    //   vector: embeddedQuery,
+    //   includeMetadata: true,
+    // });
+    // console.log(`Query Res: ${JSON.stringify(queryRes)}`);
+    // // Extract the context from the most relevant documents.
+    // context = queryRes.matches
+    //   .map((record) => record.metadata?.text)
+    //   .join("\n");
+    // console.log(`Retrieved documents: ${context}`);
+
+    //new stuff for langgraph
+    const app = createGraph();
+
+    const inputs = {
+      question: lastUserMessage.content,
+      fileId: fileId,
+    };
+
+    const result = await app?.invoke(inputs);
+
+    context = result.docs.join("\n");
   }
 
   // Format the system prompt with the context.
@@ -79,4 +113,37 @@ export async function POST(req: Request) {
   // Return the response as a streaming text response.
   const stream = OpenAIStream(response);
   return new StreamingTextResponse(stream);
+}
+
+function createGraph() {
+  try {
+    const graph = new StateGraph({
+      channels: ragState,
+    });
+
+    // Define Nodes
+    graph.addNode("retrieve", nodes.retrieve);
+    graph.addNode("grade", nodes.grade);
+    graph.addNode("transform_query", nodes.transformQuery);
+    graph.addNode("web_search", nodes.webSearch);
+
+    // Build graph with directional edges
+    graph.setEntryPoint("retrieve");
+    graph.addEdge("retrieve", "grade");
+    graph.addConditionalEdges("grade", nodes.decideToChat, {
+      transform_query: "transform_query",
+      end: END,
+    });
+    graph.addConditionalEdges("transform_query", nodes.decideToRetrieve, {
+      retrieve: "retrieve",
+      web_search: "web_search",
+    });
+    graph.addEdge("web_search", END);
+
+    const app = graph.compile();
+
+    return app;
+  } catch (e) {
+    console.error(e);
+  }
 }
